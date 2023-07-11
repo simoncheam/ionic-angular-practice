f0268b29fb74b1fcbdd569f609fc1d8f43fff071
Simon <81940817+simoncheam@users.noreply.github.com>
2023-07-11 15:14:01 -0400

    added booking modal functionality

--- src/app/bookings/bookings.module.ts
+++ src/app/bookings/bookings.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { BookingsPageRoutingModule } from './bookings-routing.module';
-
 import { BookingsPage } from './bookings.page';
 
 @NgModule({
--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { ModalController, NavController } from '@ionic/angular';
 import { PlacesService } from '../../places.service';
 import { Place } from '../../places.model';
+import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
 
 @Component({
   selector: 'app-place-detail',
@@ -10,37 +11,55 @@ import { Place } from '../../places.model';
   styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
-
   place!: Place | undefined;
-
 
   constructor(
     private route: ActivatedRoute,
     private navCtrl: NavController,
-    private placesService: PlacesService) { }
+    private placesService: PlacesService,
+    private modalCtrl: ModalController
+  ) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe( paramMap =>{
-      if(!paramMap.has('placeId')){
+    this.route.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has('placeId')) {
         //redirect via bav controller if id does not exist
         this.navCtrl.navigateBack('/places/tabs/discover');
         return;
       }
 
-      if(paramMap){
+      if (paramMap) {
         this.place = this.placesService.getPlace(paramMap.get('placeId')!);
       }
-
-
-
-
     });
   }
 
   //add onBookPlace
-  onBookPlace(){
+  public async onBookPlace() {
     // this.router.navigate(['/places/tabs/discover']);
-    this.navCtrl.navigateBack('/places/tabs/discover');
+    // this.navCtrl.navigateBack('/places/tabs/discover');
+    const modal = await this.modalCtrl.create({
+      component: CreateBookingComponent,
+      componentProps: { selectedPlace: this.place },
+    });
+
+    await modal.present();
+    const bookedPlace = await modal.onDidDismiss();
+    if (!bookedPlace.data) {
+      console.log('dismissed - no data')
+      return;
+    } else {
+      console.log('bookedPlace');
+      console.log(bookedPlace.data);
+      console.log(bookedPlace.role);
+      if (bookedPlace && bookedPlace.role ==='confirm') {
+        console.log('BOOKED');
+
+      }
+    }
   }
 
+  // public onCancel() {
+  //   this.modalCtrl.dismiss();
+  // }
 }
